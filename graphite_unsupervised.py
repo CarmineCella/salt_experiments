828d9341166907ab08f2857b9df49604f6c7a6c8
Carmine Emanuele Cella <carmine.emanuele.cella@gmail.com>
2022-05-04 17:16:46 -0700

    added shuffling

--- graphite_unsupervised.py
+++ graphite_unsupervised.py
@@ -10,7 +10,9 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.manifold import Isomap
 from sklearn.decomposition import TruncatedSVD
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from pathlib import Path
+from sklearn.utils import shuffle
 
 intensities_size = 1738
 num_samples = 6000
@@ -34,11 +36,15 @@ for i in range (1, len (sys.argv)):
     a = df.to_numpy()
     idx = (i - 1) * num_samples
     for k in range (0, num_samples - 1):
-        T[idx + k] = a[k]
+        T[idx + k] = a[k] #np.abs (np.fft.fft (a[k]))
+
+T_orig = np.copy(T)
+T = shuffle (T)
 
 # preprocessing
 print ('\nnormalizing...')
 T = preprocessing.Normalizer().fit_transform(T)
+T_orig = preprocessing.Normalizer().fit_transform(T_orig)
 
 # Clustering using KMeans
 print ('\nclustering...')
@@ -48,20 +54,24 @@ centroids, labels = kmean_model.cluster_centers_, kmean_model.labels_
 print('centroid(s):', centroids.shape)
 print('label(s)   :', labels.size)
 
-dim_method = 'isomap'
+dim_method = 'pca'
 print ('\ndimensionality reduction (' + dim_method + ')...')
 # Dimesionality reduction to 2
 if dim_method == 'isomap':
     dim_model = Isomap(n_components=2)
+    dim_model_orig = Isomap(n_components=2)
 elif dim_method == 'pca':
      dim_model = PCA(n_components=2)
+     dim_model_orig = PCA(n_components=2)
 elif dim_method == 'tsvd':
      dim_model = TruncatedSVD(n_components=2)
+     dim_model_orig = TruncatedSVD(n_components=2)
 else:
     print ('invalid reduction method')
     exit ()
 
 T = dim_model.fit_transform (T)
+T_orig = dim_model_orig.fit_transform (T_orig)
 
 # transform the 'centroids of KMean'
 centroid_dim = dim_model.transform(centroids)
@@ -76,7 +86,7 @@ features_colors = [colors[labels[i]] for i in range(len(T))]
 plt.figure(1)
 # plot the reduced components with GT colors
 for i in range (0, n_clusters):
-    plt.scatter(T[i*num_samples:i*num_samples+num_samples, 0], T[i*num_samples:i*num_samples+num_samples, 1],
+    plt.scatter(T_orig[i*num_samples:i*num_samples+num_samples, 0], T_orig[i*num_samples:i*num_samples+num_samples, 1],
                 c=colors[i], marker='.',
                 alpha=0.4, 
                 label=os.path.splitext (os.path.basename(sys.argv[i + 1]))[0]
@@ -85,7 +95,7 @@ plt.title ('Graphite: ground truth')
 plt.xlabel (dim_method + ' 1')
 plt.ylabel (dim_method + ' 2')
 plt.legend ()
-plt.savefig ('gt_' + str (n_clusters) + '_' + str (dim_method)  + '_' + str (num_samples) + '.png')
+plt.savefig ('results/gt_' + str (n_clusters) + '_' + str (dim_method)  + '_' + str (num_samples) + '.png')
 
 plt.figure (2)
 # plot the reduced components
@@ -102,13 +112,13 @@ plt.scatter(centroid_dim[:, 0], centroid_dim[:, 1],
 
 for i in range (0, n_clusters):
     plt.text(centroid_dim[i][0],
-    centroid_dim[i][1], 'class ' + str (i), 
+    centroid_dim[i][1], 'class ' + str (i + 1), 
     color='black', fontsize=14)
 
 plt.title ('Graphite: ' + str (n_clusters) + ' classe(s) - unsupervised')
 plt.xlabel (dim_method + ' 1')
 plt.ylabel (dim_method + ' 2')
-plt.savefig ('clustering_' + str (n_clusters) + '_' + str (dim_method) + '_' + str (num_samples) + '.png')
+plt.savefig ('results/clustering_' + str (n_clusters) + '_' + str (dim_method) + '_' + str (num_samples) + '.png')
 
 plt.show()
 
